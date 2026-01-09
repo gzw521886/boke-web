3b9fc1cce94cb0ea0d6c625bf91f31743a2cfc1d
ma-jx <137644548@qq.com>
2026-01-09 16:02:27 +0800

    修改网址标题和头部导航栏

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { useRef } from 'react';
 
 export default function Header() {
   const { user, isAuthenticated, logout } = useAuth();
+  const navigate = useNavigate()
 
+  const clickCount = useRef(0);
+  const timer = useRef<number | null>(null);
+
+  const handleSecretClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if (timer.current){
+      clearTimeout(timer.current);
+    }
+    clickCount.current += 1;
+    if(clickCount.current >=3){
+      clickCount.current = 0;
+      navigate('/login');
+    }else{
+      timer.current = window.setTimeout(() => {
+        clickCount.current = 0;
+      },500)
+    }
+
+  }
   return (
     <header className="border-b border-border bg-bg-primary sticky top-0 z-50">
       <div className="max-w-4xl mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
-          <Link to="/" className="text-2xl font-bold text-accent hover:text-accent-dim">
-            三三笔记
-          </Link>
+          <div>
+            <span 
+            onClick={handleSecretClick}
+            className='text-2xl font-bold text-accent hover:text-accent-dim cursor-pointer'>三</span>
+            <Link to="/" className="text-2xl font-bold text-accent hover:text-accent-dim">
+              三笔记
+            </Link>
+          </div>
 
           <nav className="flex gap-6 items-center">
             <Link to="/" className="link-accent">
@@ -36,9 +63,10 @@ export default function Header() {
                 </button>
               </>
             ) : (
-              <Link to="/login" className="link-accent">
-                登录
-              </Link>
+              <span></span>
+              // <Link to="/login" className="link-accent">
+              //   登录
+              // </Link>
             )}
           </nav>
         </div>
